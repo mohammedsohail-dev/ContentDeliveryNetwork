c43dd22eb6153944f1d818be2ee2681b5f6cbf3d
Mahalti Mohammed Sohail <75085444+mohammedsohail-dev@users.noreply.github.com>
2023-11-05 15:51:22 -0500

    Add files via upload

---
+++ cdn/src/main/java/com/contentdeliverynetwork/cdn/CdnApplication.java
@@ -0,0 +1,13 @@
+package com.contentdeliverynetwork.cdn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CdnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CdnApplication.class, args);
+	}
+
+}
